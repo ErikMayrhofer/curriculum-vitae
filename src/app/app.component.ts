d6b3d7e120ad6049e5a970cad487aaac8fd90586
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-09-23 20:35:41 +0200

    Worked on Scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as ScrollMagic from 'scrollmagic';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'Lebenslauf';
+
+  private ctrl = new ScrollMagic.Controller({addIndicators: true});
+
+  ngAfterViewInit(): void {
+    new ScrollMagic.Scene({
+      triggerElement: '.bigyeets',
+      // duration: 100,
+      // offset: 50
+    }).setPin('.bigyeets').addTo(this.ctrl);
+  }
 }
